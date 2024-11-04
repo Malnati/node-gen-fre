8f3d07821b102e95c7fe92dcbccb119dfd84914f
Malnati <ricardomalnati@gmail.com>
2024-11-04 08:08:28 -0300

    Enhance AppList component with ListActions and Edit/Show buttons; improve loading state handling

--- src/AppList.tsx
+++ src/AppList.tsx
@@ -1,6 +1,6 @@
 // src/AppList.tsx
 
-import { Datagrid, List, NumberField, ReferenceField, ReferenceInput, TextField, TextInput } from 'react-admin';
+import { Datagrid, List, NumberField, ReferenceField, ReferenceInput, TextField, TextInput, ListActions, EditButton, ShowButton } from 'react-admin';
 
 const appsFilters = [
     <TextInput source="q" label="Search" alwaysOn />,
@@ -8,19 +8,16 @@ const appsFilters = [
 ];
 
 export const AppList = () => (
-    <List filters={appsFilters}>
+    <List emptyWhileLoading filters={appsFilters} actions={<ListActions hasCreate />}>
         <Datagrid>
-            <ReferenceField source="id" reference="apps" link="show" />
             <TextField source="app" />
             <TextField source="host" />
             <NumberField source="port" />
             <TextField source="database" />
             <TextField source="user" />
-            <TextField source="password" />
-            <TextField source="outputDir" />
-            <TextField source="components" />
             <TextField source="dbType" />
-            <TextField source="id" />
+            <EditButton />
+            <ShowButton />
         </Datagrid>
     </List>
 );
