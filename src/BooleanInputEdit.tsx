c3081ff9abada8e8ea0d457f0fce79e9b2c3048c
Malnati <ricardomalnati@gmail.com>
2024-11-16 00:07:03 -0300

    Refactor BooleanInputEdit: replace Preview component with PreviewInput for improved input handling

--- src/BooleanInputEdit.tsx
+++ src/BooleanInputEdit.tsx
@@ -1,56 +1,9 @@
 // src/BooleanInputEdit.tsx
 
-import { useState, useEffect, SetStateAction } from 'react';
-import { Edit, SimpleForm, TextInput, BooleanInput, useRecordContext } from 'react-admin';
-import { Box, Paper, Typography } from '@mui/material';
+import { useState, SetStateAction } from 'react';
+import { Edit, SimpleForm, TextInput, BooleanInput } from 'react-admin';
 import { ReactNode } from 'react';
-
-interface GenericInputProps {
-    component: React.ElementType;
-    source: string;
-    current: any;
-}
-
-const GenericInput = ({ component: Component, source, current }: GenericInputProps) => {
-    return (
-        <Component
-            source={source}
-            label={current?.label || ''}
-            defaultValue={current?.defaultValue}
-            disabled={current?.disabled}
-            fullWidth={current?.fullWidth}
-            helperText={current?.helperText || ''}
-        />
-    );
-};
-
-interface PreviewProps {
-    component: React.ElementType;
-    watchedFields: any;
-}
-
-const Preview = ({ component: Component, watchedFields }: PreviewProps) => {
-
-    const record = useRecordContext();
-    const [current, setCurrent] = useState(record);
-    
-    useEffect(() => {
-        setCurrent(watchedFields);
-    }, [watchedFields]);
-
-    if (!current) return null;
-
-    return (
-        <>
-            <Paper elevation={3} sx={{ padding: '15px', margin: '15px', width: '98%' }}>
-            <Typography variant="h6" sx={{ padding: '15px' }}>Changes view</Typography>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <GenericInput source="genericInputs" component={Component} current={current} />
-                </Box>
-            </Paper>
-        </>
-    );
-};
+import PreviewInput from './PreviewInput';
 
 export const BooleanInputEdit = ({ children }: { children?: ReactNode }) => {
 
@@ -205,7 +158,7 @@ export const BooleanInputEdit = ({ children }: { children?: ReactNode }) => {
         <>
             <Edit>
                 <SimpleForm>
-                    <Preview watchedFields={watchedFields} component={BooleanInput}/>
+                    <PreviewInput watchedFields={watchedFields} component={BooleanInput}/>
                     <TextInput source="id" label="Id" disabled onChange={handleOnChangeId} />
                     <TextInput source="helperText" label="Helper Text" onChange={handleOnChangeHelperText} />
                     <TextInput source="label" label="Label" onChange={handleOnChangeLabel}/>
