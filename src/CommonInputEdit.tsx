3a5ac1930161c113d06af2129e7389b4917be70b
Malnati <ricardomalnati@gmail.com>
2024-11-22 13:55:02 -0300

    Replace useInputChanges hook with useObserveChanges for improved state management and field observation

--- src/CommonInputEdit.tsx
+++ src/CommonInputEdit.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from 'react';
 import { Edit, SimpleForm, TextInput, BooleanInput, CheckboxGroupInput } from 'react-admin';
 import PreviewInput from './PreviewEditInput';
-import useInputChanges from './hooks/useInputChanges';
+import useObserveChanges from './hooks/useObserveChanges';
 import PreviewEditCheckboxInput from './PreviewEditCheckboxInput';
 
 interface CommonInputEditProps {
@@ -14,28 +14,28 @@ interface CommonInputEditProps {
 export const CommonInputEdit = ({ children, component: Component }: CommonInputEditProps) => {
 
     const {
-        changedFields,
-        handleOnChange
-    } = useInputChanges();
+        observedFields,
+        observeIt
+    } = useObserveChanges();
 
     return (
         <>
             <Edit>
                 <SimpleForm>
                     {Component === CheckboxGroupInput && 
-                        <PreviewEditCheckboxInput watchedFields={changedFields} />}
+                        <PreviewEditCheckboxInput watchedFields={observedFields} />}
                     {Component !== CheckboxGroupInput && 
-                        <PreviewInput watchedFields={changedFields} component={Component} />}
-                    <TextInput source="id" label="Id" disabled onChange={(e) => handleOnChange('id', e.target.value)} />
-                    <TextInput source="helperText" label="Helper Text" onChange={(e) => handleOnChange('helperText', e.target.value)} />
-                    <TextInput source="label" label="Label" onChange={(e) => handleOnChange('label', e.target.value)}/>
+                        <PreviewInput watchedFields={observedFields} component={Component} />}
+                    <TextInput source="id" label="Id" disabled onChange={(e) => observeIt('id', e.target.value)} />
+                    <TextInput source="helperText" label="Helper Text" onChange={(e) => observeIt('helperText', e.target.value)} />
+                    <TextInput source="label" label="Label" onChange={(e) => observeIt('label', e.target.value)}/>
                     {Component !== CheckboxGroupInput && 
-                        <BooleanInput source="defaultValue" label="Default Value" onChange={(e) => handleOnChange('defaultValue', e.target.value)} />}
-                    <BooleanInput source="readOnly" label="Read Only" onChange={(e) => handleOnChange('readOnly', e.target.value)} />
-                    <BooleanInput source="disabled" label="Disabled" onChange={(e) => handleOnChange('disabled', e.target.value)} />
-                    <BooleanInput source="fullWidth" label="Full Width" onChange={(e) => handleOnChange('fullWidth', e.target.value)} />
-                    <TextInput source="source" label="Source" onChange={(e) => handleOnChange('source', e.target.value)} />
-                    <TextInput source="className" label="ClassName" onChange={(e) => handleOnChange('className', e.target.value)} />
+                        <BooleanInput source="defaultValue" label="Default Value" onChange={(e) => observeIt('defaultValue', e.target.value)} />}
+                    <BooleanInput source="readOnly" label="Read Only" onChange={(e) => observeIt('readOnly', e.target.value)} />
+                    <BooleanInput source="disabled" label="Disabled" onChange={(e) => observeIt('disabled', e.target.value)} />
+                    <BooleanInput source="fullWidth" label="Full Width" onChange={(e) => observeIt('fullWidth', e.target.value)} />
+                    <TextInput source="source" label="Source" onChange={(e) => observeIt('source', e.target.value)} />
+                    <TextInput source="className" label="ClassName" onChange={(e) => observeIt('className', e.target.value)} />
                     {children}
                 </SimpleForm>
             </Edit>
