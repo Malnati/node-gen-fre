2a4c73d93718cd9e27262afc2384b504de773fa6
Malnati <ricardomalnati@gmail.com>
2024-12-04 16:02:23 -0300

    Add CheckboxInputEdit component using CommonInputCreate for checkbox group handling

---
+++ src/resource/CheckboxInputCreate.tsx
@@ -0,0 +1,12 @@
+// src/resource/CheckboxInputCreate.tsx
+
+import { CheckboxGroupInput } from 'react-admin';
+import { CommonInputCreate } from '../components/CommonInputCreate';
+
+export const CheckboxInputEdit = () => {
+
+    return (
+        <CommonInputCreate component={CheckboxGroupInput} />
+    );
+};
+
