519f01f0e4fe0e1379dbe0dc961535ac7fa6c424
Malnati <ricardomalnati@gmail.com>
2024-12-04 15:58:55 -0300

    Refactor BooleanInputCreate to use CommonInputCreate for simplified form handling

--- src/resource/BooleanInputCreate.tsx
+++ src/resource/BooleanInputCreate.tsx
@@ -1,19 +1,8 @@
 // src/resource/BooleanInputCreate.tsx
 
-import { BooleanInput, Create, SimpleForm, TextInput } from 'react-admin';
+import { BooleanInput } from 'react-admin';
+import { CommonInputCreate } from '../components/CommonInputCreate';
 
 export const BooleanInputCreate = () => (
-    <Create>
-        <SimpleForm>
-            <TextInput source="id" label="Id" />
-            <TextInput source="source" label="Source" />
-            <TextInput source="className" label="ClassName" />
-            <BooleanInput source="defaultValue" label="Default Value" />
-            <BooleanInput source="readOnly" label="Read Only" />
-            <BooleanInput source="disabled" label="Disabled" />
-            <BooleanInput source="fullWidth" label="Full Width" />
-            <TextInput source="helperText" label="Helper Text" />
-            <TextInput source="label" label="Label" />
-        </SimpleForm>
-    </Create>
+    <CommonInputCreate component={BooleanInput} />
 );
