b9c31609742677c040776fe9d3c4f719912679ba
Malnati <ricardomalnati@gmail.com>
2024-12-05 09:38:01 -0300

    feat: Move AvatarCreate component to 'backlog' directory and update import paths for input components

--- src/resource/input/BooleanInputCreate.tsx
+++ src/resource/input/BooleanInputCreate.tsx
@@ -1,7 +1,7 @@
 // src/resource/BooleanInputCreate.tsx
 
 import { BooleanInput } from 'react-admin';
-import { CommonInputCreate } from '../components/CommonInputCreate';
+import { CommonInputCreate } from '../../components/CommonInputCreate';
 
 export const BooleanInputCreate = () => (
     <CommonInputCreate component={BooleanInput} />
--- src/resource/input/CheckboxInputCreate.tsx
+++ src/resource/input/CheckboxInputCreate.tsx
@@ -1,7 +1,7 @@
 // src/resource/CheckboxInputCreate.tsx
 
 import { CheckboxGroupInput } from 'react-admin';
-import { CommonInputCreate } from '../components/CommonInputCreate';
+import { CommonInputCreate } from '../../components/CommonInputCreate';
 
 export const CheckboxInputCreate = () => {
 
